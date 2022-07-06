bacf0ef6a3e8fcc9619ae0e334eefde85c1116a3
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-07-06 17:13:34 +0530

    Update PositiveNegativeNumbers.java

--- PositiveNegativeNumbers.java
+++ PositiveNegativeNumbers.java
@@ -1,3 +1,4 @@
+// Using If Else Case
 import java.util.Scanner;
 public class PositiveNegativeNumbers {
     public static void main(String[] args) {
@@ -12,6 +13,24 @@ public class PositiveNegativeNumbers {
         }
         else{
             System.out.println("Entered Number is Negative");
+        }
+    }
+}
+
+// Using Ternary Operator
+
+import java.util.Scanner;
+public class PositiveNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter any Number:");
+        int n = sc.nextInt();
+        if(n>0){
+            System.out.println("Entered Number is Positive");
+        }
+        else{
+            String result = n<0 ? "Number is Negative" : "Number is Zero";
+            System.out.println(result);
         }
     }
 }
