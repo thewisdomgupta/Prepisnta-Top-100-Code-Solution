0a6b105850012a219ba179553506ff2a7043d3a7
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-07-06 17:00:23 +0530

    Update PositiveNegativeNumbers.java

--- PositiveNegativeNumbers.java
+++ PositiveNegativeNumbers.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 public class PositiveNegativeNumbers {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter any Number:");
         int n = sc.nextInt();
         if(n>0){
             System.out.println("Entered Number is Positive");
+        }
+        else if(n==0){
+            System.out.println("Entered Number is Zero");
         }
         else{
             System.out.println("Entered Number is Negative");
