3623378c2024adb941c7846a50ff26dedd0c9dff
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-07-06 17:37:36 +0530

    Update EvenOrOddNumbers.java

--- EvenOrOddNumbers.java
+++ EvenOrOddNumbers.java
@@ -13,3 +13,17 @@ public class EvenOrOddNumbers {
         }
     }
 }
+
+
+// Using Teranary Operator
+import java.util.Scanner;
+public class EvenOrOddNumbers {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any Number:");
+        int n = sc.nextInt();
+        String Result = n%2==0? "Number is Even" : "Number is Odd";
+        System.out.println(Result);
+
+    }
+}
