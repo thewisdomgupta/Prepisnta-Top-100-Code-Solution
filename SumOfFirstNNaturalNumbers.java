5dea872cb3278f192accc412249b9c2b4cc5e267
Vivek Kumar Gupta <100516335+thewisdomgupta@users.noreply.github.com>
2022-07-06 17:51:28 +0530

    Update SumOfFirstNNaturalNumbers.java

--- SumOfFirstNNaturalNumbers.java
+++ SumOfFirstNNaturalNumbers.java
@@ -13,3 +13,17 @@ public class SumOfFirstNNaturalNumbers {
         System.out.println(total);
     }
 }
+
+
+// Sum of First N Natural Numbers
+import java.util.Scanner;
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String [] args){
+    Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Any Number:");
+        int num = sc.nextInt();
+        int result = (num*(num+1))/2;
+        System.out.println("Total Sum is");
+        System.out.println(result);
+    }
+}
